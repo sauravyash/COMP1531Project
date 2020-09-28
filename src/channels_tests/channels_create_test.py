cc7b51d89f042eb7653b6f406dee6876d1b2bdb9
Yash <yagasti@gmail.com>
2020-09-28 20:38:31 +1000

    Add channel_create test requirements into test files

---
+++ src/channels_tests/channels_create_test.py
@@ -0,0 +1,35 @@
+import echo
+import pytest
+from error import InputError
+
+# Test for Valid Token
+# - Token must be a string
+# - Token cannot be empty
+# - Token must be a number
+# - Token must be unique
+def test_channels_create_valid_token():
+    pass
+
+# Test for Valid Name
+# - Name must be a string
+# - Name cannot be an empty String
+# - Name cannot be longer than 20 chars
+def test_channels_create_valid_name():
+    pass
+
+# Test channel privacy
+# - Is_Public is a boolean
+def test_channels_create_is_public():
+    pass
+
+# Test channel admin status
+def test_channels_create_is_empty():
+    pass
+
+# Test for admin leaving group
+def test_channels_create_token():
+    pass
+
+
+
+
