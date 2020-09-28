c90d41b9c345c54bc7b5bc40f39c671603f51dc5
Yash <yagasti@gmail.com>
2020-09-28 22:46:49 +1000

    Complete 'channels_create' blackbox tests

--- src/channels_tests/channels_create_test.py
+++ src/channels_tests/channels_create_test.py
@@ -1,34 +1,51 @@
-import echo
 import pytest
 from error import InputError
+from channels import channels_create
+
+### BLACKBOX TESTING ###
 
 # Test for Valid Token
 # - Token must be a string
 # - Token cannot be empty
-# - Token must be a number
 # - Token must be unique
 def test_channels_create_valid_token():
-    pass
+    with pytest.raises(InputError) as e:
+        channels_create(0, "namee", true)
+    with pytest.raises(InputError) as e:
+        channels_create("", "namee", true)
+    with pytest.raises(InputError) as e:
+        channels_create(None, "namee", true)
+
 
 # Test for Valid Name
 # - Name must be a string
 # - Name cannot be an empty String
 # - Name cannot be longer than 20 chars
-def test_channels_create_valid_name():
-    pass
+def test_channels_create_valid_name(): 
+    with pytest.raises(InputError) as e:
+        channels_create("123421", None, true)
+    with pytest.raises(InputError) as e:
+        channels_create("234234", "", true)
+    with pytest.raises(InputError) as e:
+        # 21 char length name
+        channels_create("233422", "qwertyuiopasdfghjklzx", true)
 
 # Test channel privacy
-# - Is_Public is a boolean
+# - is_public is a boolean
 def test_channels_create_is_public():
-    pass
+    with pytest.raises(InputError) as e:
+        channels_create("123421", "nbamee", None)
+    with pytest.raises(InputError) as e:
+        channels_create("234234", "dfdfdfs", 0)
+    with pytest.raises(InputError) as e:
+        # 21 char length name
+        channels_create("233422", "qweklzx", "hi")
 
-# Test channel admin status
-def test_channels_create_is_empty():
-    pass
+# Test function output types
+# - message_id is an int
+def test_channels_create_check_return_types():
+    assert isinstance(channels_create("", "", true), int)
 
-# Test for admin leaving group
-def test_channels_create_token():
-    pass
 
 
 
