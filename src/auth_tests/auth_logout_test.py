0dce395242363bf275f0ab697b902b0ac52eaab7
Gabriel Rosendo <g.rosendo@student.unsw.edu.au>
2020-09-29 13:33:16 +1000

    fixed imports and logout tests passing pipeline

--- src/auth_tests/auth_logout_test.py
+++ src/auth_tests/auth_logout_test.py
@@ -1,17 +1,15 @@
-from auth import auth_register
-from auth import auth_login
-from auth import auth_logout
+import auth
 import pytest
 
 #Successful logouts
 def test_logout():
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     auth.auth_login('validemail@gmail.com', 'validpassword')
-    auth.auth_logout(result[token])
+    auth.auth_logout(result)
 
 #Unsuccesful logouts
 def test_unregisteredemail():
     result = auth.auth_register('validemail1@gmail.com', 'validpassword', 'fname', 'fname')
     auth.auth_login('validemail1@gmail.com', 'validpassword')
     #invalid token (unvalidated token)
-    auth.auth_logout(result[token])
+    auth.auth_logout(result)
