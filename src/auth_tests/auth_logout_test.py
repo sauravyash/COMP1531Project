c59a538f5da08c16d34f155a2c81be7e4cee371b
Gabriel Rosendo <g.rosendo@student.unsw.edu.au>
2020-09-29 10:21:49 +1000

    updated logout, error found in imports

--- src/auth_tests/auth_logout_test.py
+++ src/auth_tests/auth_logout_test.py
@@ -1,17 +1,17 @@
 from auth import auth_register
 from auth import auth_login
-form auth import auth_logout
+from auth import auth_logout
 import pytest
 
 #Successful logouts
 def test_logout():
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     auth.auth_login('validemail@gmail.com', 'validpassword')
-    auth.auth_logout(result[1])
+    auth.auth_logout(result[token])
 
 #Unsuccesful logouts
 def test_unregisteredemail():
-    result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
-    auth.auth_login('validemail@gmail.com', 'validpassword')
+    result = auth.auth_register('validemail1@gmail.com', 'validpassword', 'fname', 'fname')
+    auth.auth_login('validemail1@gmail.com', 'validpassword')
     #invalid token (unvalidated token)
     auth.auth_logout(result[token])
