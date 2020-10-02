b1b0e3840cafdc1029cd265a742da514669b8363
Yash <yagasti@gmail.com>
2020-10-03 02:21:29 +1000

    Finished channel create with unique id generator

--- src/channels.py
+++ src/channels.py
@@ -22,13 +22,21 @@ def channels_listall(token):
 
 def channels_create(token, name, is_public):
     channel_id = 0 
+    unique = False
+    while unique == False:
+        try:
+            data.resolve_channel_id_index(channel_id)
+            channel_id += 1
+        except LookupError:
+            unique == True
+
     data.data['channels'].append({
         'id': channel_id,
         'name' : name,
         'admins': [data.resolve_token(token)],
-        'members': [] ,
-        'messages' []
-        'is_public': True
+        'members': [],
+        'messages' [],
+        'is_public': is_public
     })
     return {
         'channel_id': channel_id,
