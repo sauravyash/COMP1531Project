e13d63ae16e394d89eb071cbb51d1c97c2e70190
Alex <z5260120@ad.unsw.edu.au>
2020-10-03 22:52:25 +1000

    update channel_addowner and tests

--- src/channel_addowner_test.py
+++ src/channel_addowner_test.py
@@ -1,23 +1,25 @@
 #############################################Channel Addowner Tests###############################
 from channel import channel_addowner
 import pytest
+from error import AccessError
+from error import InputError
 # Success Addowner
 def test_addowner():
-    channel.channel_addowner("VALID token", "VALID chID", "VALID uID")
+    assert (channel_addowner("VALID token", 1, 1)) == {}
 
 # Fail Addowner
 def test_addowner_invalid_token():
     with pytest.raises(InputError) as e:
-        channel.channel_addowner("INVALID token", "VALID chID", "VALID uID")
+        channel_addowner("INVALID token", 2, 2)
 
 def test_addowner_invalid_chID():
     with pytest.raises(InputError) as e:
-        channel.channel_addowner("VALID token", "INVALID chID", "VALID uID")
+        channel_addowner("VALID token", 3, 3)
 
 def test_addowner_invalid_uID():
     with pytest.raises(InputError) as e:
-        channel.channel_addowner("VALID token", "VALID chID", "INVALID uID")
+        channel_addowner("VALID token", 4, 4)
 
 def test_addowner_not_authorised ():
-    with pytest.raises(AcessError) as e:
-        channel.channel_addowner("VALID token", "VALID chID", "VALID chID", "User is not a member or User is already an owner")
+    with pytest.raises(AccessError) as e:
+        channel_addowner("VALID token", 5, 5)
