73723bb541e31c656c37e62c25dac839b132bb98
Gabriel Rosendo <g.rosendo@student.unsw.edu.au>
2020-10-03 21:41:21 +1000

    added tests for empty cases in auth_login_test

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -4,26 +4,38 @@ import pytest
 from error import InputError
 
 #Successful logins
-def test_validemail():
+def test_success():
     other.clear()
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     auth.auth_login('validemail@gmail.com', 'validpassword')
 
 #Unsuccesful logins
-def test_unregisteredemail():
+def test_unregistered_email():
     other.clear()
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     with pytest.raises(InputError) as e:
         auth.auth_login('unregisteredemail@gmail.com', 'validpassword')
 
-def test_incorrectpassword():
+def test_incorrect_password():
     other.clear()
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     with pytest.raises(InputError) as e:
         auth.auth_login('validemail@gmail.com', 'notthecorrectpassword')
 
-def test_invalidemail():
+def test_invalid_email():
     other.clear()
     result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
     with pytest.raises(InputError) as e:
         auth.auth_login('invalidemail.com', 'validpassword')
+
+def test_empty_email():
+    other.clear()
+    result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
+    with pytest.raises(InputError) as e:
+        auth.auth_login('', 'validpassword')
+
+def test_empty_password():
+    other.clear()
+    result = auth.auth_register('validemail@gmail.com', 'validpassword', 'fname', 'fname')
+    with pytest.raises(InputError) as e:
+        auth.auth_login('validemail@gmail.com', '')
