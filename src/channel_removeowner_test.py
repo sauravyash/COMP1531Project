ef1e3432534a455056e3d2bf84cf6a7ac6ec3da5
Alex <z5260120@ad.unsw.edu.au>
2020-10-03 23:26:47 +1000

    update channel_removeowner function and update tests

--- src/channel_removeowner_test.py
+++ src/channel_removeowner_test.py
@@ -1,23 +1,25 @@
 #############################################Channel Removeowner Tests############################
 from channel import channel_removeowner
+from error import AccessError
+from error import InputError
 import pytest
 # Success removeowner
 def test_removeowner():
-    channel.channel_removeowner("VALID token", "VALID chID", "VALID uID")
+    channel_removeowner("VALID token", 6, 6)
 
 # Fail Removeowner
 def test_removeowner_invalid_token():
     with pytest.raises(InputError) as e:
-        channel.channel_removeowner("INVALID token", "VALID chID", "VALID uID")
+        channel_removeowner("INVALID token", 7, 7)
     
 def test_removeowner_invalid_chID():
     with pytest.raises(InputError) as e:
-        channel.channel_removeowner("VALID token", "INVALID chID", "VALID uID")
+        channel_removeowner("VALID token", 8, 8)
 
 def test_removeowner_invalid_uID():
     with pytest.raises(InputError) as e:
-        channel.channel_removeowner("VALID token", "VALID chID", "INVALID uID")
+        channel_removeowner("VALID token", 9, 9)
 
 def test_removeowner_not_authorised():
-    with pytest.raises(AcessError) as e:
-        channel.channel_removeowner("VALID token", "VALID chID", "VALID uID", "User does not authorised", "Target is the owner")
+    with pytest.raises(AccessError) as e:
+        channel_removeowner("VALID token", 10, 10)
