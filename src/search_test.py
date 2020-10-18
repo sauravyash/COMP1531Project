5cbf3a5a80a23d97b91780cc6212624c10537ba2
Arzaan Mahiar Mirza <z5311529@ad.unsw.edu.au>
2020-10-18 16:14:50 +1100

    tests for search

---
+++ src/search_test.py
@@ -0,0 +1,16 @@
+from channel import channel_invite
+from channel import channel_messages
+from error import InputError
+from error import AccessError
+import auth
+import channels
+import other
+import pytest
+import random
+import string
+
+
+def test_valid():
+
+
+    assert search("valid_token","this is a query string") == {"A collection of messages"}
