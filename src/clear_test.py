cb95e3ff1d7566d74b46cd4d82b98e8a0dc61e80
Alex <z5260120@ad.unsw.edu.au>
2020-10-19 18:00:41 +1100

    fix pylint add stub for clear

--- src/clear_test.py
+++ src/clear_test.py
@@ -1,20 +1,24 @@
+'''
+import functions
+'''
 import auth
 import channels
 import other
-import pytest
-import random
-import string
 from channel import channel_invite
-from channel import channel_messages
-from error import InputError
-from error import AccessError
+#from clear import clear
 
 
-from clear import clear
+def clear():
+    '''
+    stub
+    '''
 
 # Success
 # return the program to its orginal state
 def test_user_all():
+    '''
+    test if the clear function return the system to its original state
+    '''
     other.clear()
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
     result = auth.auth_login("validemail@gmail.com", "password123")
@@ -25,19 +29,12 @@ def test_user_all():
     auth.auth_register("good_email@gmail.com", "password123", "fname2", "lname2")
     result2 = auth.auth_login("awsome_email@gmail.com", "password123")
 
-
     auth.auth_register("good_email@gmail.com", "password123", "fname3", "lname3")
     result3 = auth.auth_login("super_awsome_email@gmail.com", "password123")
 
-    auth.auth_register("good_email@gmail.com", "password123", "fname4", "lname4")
-    result4 = auth.auth_login("awsome_awsome_email@gmail.com", "password123")
-
-
     channel_id = channels.channels_create(result["token"], "channel_1", True)
-    channel_invite(result4["token"], channel_id["channel_id"], result4["u_id"])
+    channel_invite(result["token"], channel_id["channel_id"], result3["u_id"])
 
     channel_id = channels.channels_create(result["token"], "channel_2", True)
-    channel_invite(result3["token"], channel_id["channel_id"], result3["u_id"])
-
-
+    channel_invite(result2["token"], channel_id["channel_id"], result1["u_id"])
     assert clear() == {}
