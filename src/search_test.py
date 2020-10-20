5d1b80f5ca1414b55acd0fd51c54bb835ad19020
Tara Andresson <z5255646@ad.unsw.edu.au>
2020-10-21 00:34:16 +1100

    Fixed tests for search function

--- src/search_test.py
+++ src/search_test.py
@@ -15,19 +15,70 @@ def create_test_channel():
     create test channel for messages
     '''
     other.clear()
+    
+    # Register and login two users...
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
-    result = auth.auth_login("validemail@gmail.com", "password123")
+    result1 = auth.auth_login("validemail@gmail.com", "password123")
 
     auth.auth_register("newvalidemail@gmail.com", "password123", "fname1", "lname1")
-    result1 = auth.auth_login("newvalidemail@gmail.com", "password123")
-
-    channel_id = channels.channels_create(result["token"], "channel_1", True)
-    channel_invite(result["token"], channel_id["channel_id"], result1["u_id"])
-
-    return (result, result1, channel_id)
-
-def test_valid_search():
-    result, result1, channel_id = create_test_channel()
+    result2 = auth.auth_login("newvalidemail@gmail.com", "password123")
     
-    message.message_send(result, channel_id, "hello")
+    # Create a channel and invite a user to it...
+    channel = channels.channels_create(result1['token'], 'channel_1', True)
+    channel_invite(result1['token'], channel_id['channel_id'], result2['u_id'])
+
+    return (result['token'], result1['token'], channel['channel_id'])
+
+def test_simple_search():
+    # Create test environment...
+    token1, token2, channel_id = create_test_channel()
     
+    # Send messages...
+    mid_1 = message.message_send(token1, channel_id, 'hello')
+    mid_2 = message.message_send(token2, channel_id, 'help me')
+    mid_3 = message.message_send(token1, channel_id, 'what the hell')
+    
+    # Test search...
+    return_messages = other.search(token1, 'he')
+    assert return_messages['messages'][1]['message_id'] == mid_1
+    assert return_messages['messages'][2]['message_id'] == mid_2
+    assert return_messages['messages'][3]['message_id'] == mid_3
+    
+def test_selective_search():
+    # Create test environment...
+    token1, token2, channel_id = create_test_channel()
+    
+    # Send messages...
+    mid_1 = message.message_send(token1, channel_id, 'hello')
+    mid_2 = message.message_send(token2, channel_id, 'help me')
+    mid_3 = message.message_send(token1, channel_id, 'what the hell')
+    
+    # Test search...
+    return_messages1 = other.search(token1, 'hell')
+    assert return_messages1['messages'][1]['message_id'] == mid_1
+    assert return_messages1['messages'][2]['message_id'] == mid_3
+    
+    return_messages2 = other.search(token1, 'what the')
+    assert return_messages2['messages'][1]['message_id'] == mid_3
+
+def test_diff_users():
+    
+    # Create test environment...
+    token1, token2, channel_id = create_test_channel()
+    
+    # Send messages...
+    mid_1 = message.message_send(token1, channel_id, 'hello')
+    mid_2 = message.message_send(token2, channel_id, 'help me')
+    mid_3 = message.message_send(token1, channel_id, 'what the hell')
+    
+    # Test search...
+    return_messages1 = other.search(token1, 'hell')
+    assert return_messages1['messages'][1]['message_id'] == mid_1
+    assert return_messages1['messages'][2]['message_id'] == mid_3
+    
+    return_messages2 = other.search(token2, 'hell')
+    assert return_messages1['messages'][1]['message_id'] == mid_1
+    assert return_messages1['messages'][2]['message_id'] == mid_3
+    
+    assert other.search('fake_token', 'hell') == {'messages': []}
+
