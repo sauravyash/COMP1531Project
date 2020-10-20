39578bd3e48b10c22476aa932c343ce722bcd4a6
Yash <yagasti@gmail.com>
2020-10-20 23:58:35 +1100

    start test file for search

--- src/search_test.py
+++ src/search_test.py
@@ -1,17 +1,33 @@
-'''from channel import channel_invite
+from channel import channel_invite
 from channel import channel_messages
 from error import InputError
 from error import AccessError
 import auth
 import channels
+import message
 import other
 import pytest
 import random
 import string
 
+def create_test_channel():
+    '''
+    create test channel for messages
+    '''
+    other.clear()
+    auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
+    result = auth.auth_login("validemail@gmail.com", "password123")
 
-def test_valid():
+    auth.auth_register("newvalidemail@gmail.com", "password123", "fname1", "lname1")
+    result1 = auth.auth_login("newvalidemail@gmail.com", "password123")
 
+    channel_id = channels.channels_create(result["token"], "channel_1", True)
+    channel_invite(result["token"], channel_id["channel_id"], result1["u_id"])
 
-    assert search("valid_token","this is a query string") == {"A collection of messages"}
-'''
+    return (result, result1, channel_id)
+
+def test_valid_search():
+    result, result1, channel_id = create_test_channel()
+    
+    message.message_send(result, channel_id, "hello")
+    
