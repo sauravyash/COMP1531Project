938f9e8fbaf305edadc0625ce83984682fb965e7
Tara Andresson <z5255646@ad.unsw.edu.au>
2020-10-21 00:38:26 +1100

    added search function after implementing testing

--- src/other.py
+++ src/other.py
@@ -38,13 +38,22 @@ def admin_userpermission_change(token, u_id, permission_id):
     pass
 
 def search(token, query_str):
+    
+    list_of_messages = []
+    list_of_channels = channels_list(token)
+    
+    # Go through the relevant channels...
+    for channel in list_of_channels:
+        index = data.resolve_channel_id_index(channel['channel_id'])
+        # Test if the user is a member or admin of the channel (has joined)
+        selected_channel = data.data['channels'][index]
+        if token in selected_channel[members] or token in selected_channel[admin]:
+            for message_info in selected_channel['messages']:
+                if query_str in message['message']:
+                    # if the query string is contained within a message, add that
+                    # message and its details to a list.
+                    list_of_messages.append(message_info)
+    
     return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
+        'messages': [list_of_messages]
     }
