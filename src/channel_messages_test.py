7ed61fb98cd3fba6637755a4abb689855f7b54ce
Alex <z5260120@ad.unsw.edu.au>
2020-10-21 13:45:56 +1100

    update channel_messages_test

--- src/channel_messages_test.py
+++ src/channel_messages_test.py
@@ -1,15 +1,19 @@
-#######################################Channel Messages Test#############################
+'''Channel message test'''
+import pytest
+import auth
+import channels
+import other
+
 from channel import channel_invite
 from channel import channel_messages
 from error import InputError
 from error import AccessError
-import auth
-import channels
-import other
-import pytest
+from message import message_send
+
 
 # Success Messages
 def test_valid_channel_message():
+    '''Success Messages'''
     other.clear()
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
     result = auth.auth_login("validemail@gmail.com", "password123")
@@ -19,15 +23,16 @@ def test_valid_channel_message():
 
     channel_id = channels.channels_create(result["token"], "channel_1", True)
     channel_invite(result1["token"], channel_id["channel_id"], result1["u_id"])
-
-    assert channel_messages(result1["token"], channel_id["channel_id"], 0) == {
-        'messages': [],
-        'start': 0,
-        'end': -1,
-    }
+    message_send(result1["token"], channel_id["channel_id"], "Hello There")
+    check = channel_messages(result1["token"], channel_id["channel_id"], 0)
+    assert check["end"] == -1
+    assert check["start"] == 0
+    assert check["messages"][0]["u_id"] == 2
+    assert check["messages"][0]["message"] == "Hello There"
 
 # Fail
-def test_invalid_channel_messages_ID():
+def test_invalid_channel_messages_id():
+    ''' Invalid Channel ID'''
     other.clear()
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
     result = auth.auth_login("validemail@gmail.com", "password123")
@@ -40,9 +45,10 @@ def test_invalid_channel_messages_ID():
     channel_invite(result1["token"], channel_id["channel_id"], result1["u_id"])
 
     with pytest.raises(InputError):
-        channel_messages(result1["token"], -1, 0)
+        channel_messages(result1["token"], 15, 0)
 
 def test_invalid_channel_messages_start():
+    ''' Invalid Start'''
     other.clear()
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
     result = auth.auth_login("validemail@gmail.com", "password123")
@@ -58,6 +64,7 @@ def test_invalid_channel_messages_start():
         channel_messages(result1["token"], channel_id["channel_id"], -1)
 
 def test_invalid_channel_messages_authorised():
+    ''' not a member of the channel'''
     other.clear()
     auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
     result = auth.auth_login("validemail@gmail.com", "password123")
