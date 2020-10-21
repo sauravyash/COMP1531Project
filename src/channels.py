365287737e5d3cf1276f1af6311fcd20af79138b
Tara Andresson <z5255646@ad.unsw.edu.au>
2020-10-21 13:05:31 +1100

    fixed new data structure to match permission_id's

--- src/channels.py
+++ src/channels.py
@@ -16,7 +16,7 @@ def channels_list(token):
 
     # Flockr owner can see all channels...
     for user in data.data['users']:
-        if user['flockr_owner'] == True and user['id'] == user_id:
+        if user['flockr_owner'] and user['id'] == user_id:
             return channels_listall(token)
 
     # Otherwise, only the channels they are a member of...
@@ -34,9 +34,9 @@ def channels_list(token):
 
 def channels_listall(token):
 
+    # Check that token is valid, if so return list of all channels...
     try:
         new_list = []
-
         data.resolve_token(token)
 
         for channel in data.data['channels']:
@@ -48,37 +48,47 @@ def channels_listall(token):
 
         return new_list
 
-    # If token doesn't exist...
+    # If token doesn't exist, return empty list...
     except:
         return []
 
 def channels_create(token, name, is_public):
+    
+    # Check that token exists/ is valid.
     try:
         data.resolve_token(token)
     except LookupError: # pragma: no cover
         raise AccessError("Invalid Token")
+    
+    # Check if name param is valid.
     if not isinstance(name, str) or len(name) > 20 or len(name) < 1:
         raise InputError("Invalid Name")
+    # Check if is_public param is valid.
     if not isinstance(is_public, bool):
         raise InputError("Invalid is_public variable")
-
+    
+    # Create and check that channel id is unique... (to be fixed)
     channel_id = 0
     unique = False
-    while unique == False:
+    while not unique:
         try:
             data.resolve_channel_id_index(channel_id)
             channel_id += 1
         except LookupError:
             unique = True
 
+    # Create channel data structure & add new information...
     data.data['channels'].append({
         'id': channel_id,
         'name' : name,
-        'admins': [data.resolve_token(token)],
-        'members': [],
+        'members': {
+            'permission_id_1': [data.resolve_token(token)],
+            'permission_id_2': [],
+        }
         'messages': [],
         'is_public': is_public
     })
+    
     return {
         'channel_id': channel_id,
     }
