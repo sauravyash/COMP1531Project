5142ccee41af7a5452757d05066fb0f1b6525c28
Alex <z5260120@ad.unsw.edu.au>
2020-11-02 21:36:52 +1100

    new user upload photo test

---
+++ src/user_profile_uploadphoto.py
@@ -0,0 +1,46 @@
+'''Import functions'''
+import pytest
+import auth
+import channel
+import channels
+import other
+from error import InputError
+from error import AccessError
+from user import user_profile_uploadphoto
+
+####### NOTE ###########
+
+#InputError- when img_url returns an HTTP status other than 200.
+
+########################
+
+def test_user_profile_uploadphoto():
+    ''' Success upload photo'''
+    other.clear()
+
+    auth.auth_register("coolemail@gmail.com", "password123", "fname", "lname")
+    result = auth.auth_login("coolemail@gmail.com", "password123")
+
+    assert(user_profile_uploadphoto(result["token"], "https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.jpg", 0, 0, 256, 256)) == {}
+
+
+def test_not_jpg():
+    ''' Image URL is not jpg'''
+    other.clear()
+
+    auth.auth_register("coolemail@gmail.com", "password123", "fname", "lname")
+    result = auth.auth_login("coolemail@gmail.com", "password123")
+
+    with pytest.raises(InputError):
+        user_profile_uploadphoto(result["token"], "https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.png", 0, 0, 256, 256)
+
+
+def test_invalid_demension():
+    '''Invalid Demension'''
+    other.clear()
+
+    auth.auth_register("coolemail@gmail.com", "password123", "fname", "lname")
+    result = auth.auth_login("coolemail@gmail.com", "password123")
+
+    with pytest.raises(InputError):
+        user_profile_uploadphoto(result["token"], "https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.jpg", -1, -1, 256, 256)
