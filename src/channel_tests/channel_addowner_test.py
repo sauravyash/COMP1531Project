f5810d31acfc532f15956fb14eaeccc84217b397
Tara Andresson <z5255646@ad.unsw.edu.au>
2020-11-05 12:50:44 +1100

    some bug in channel_addowner where input is invalid (can't find channel) also added fixture to channels create tests

--- src/channel_tests/channel_addowner_test.py
+++ src/channel_tests/channel_addowner_test.py
@@ -10,6 +10,7 @@ from error import InputError
 from channel import channel_addowner
 from channel import channel_invite
 from channel import channel_details
+from channel import channel_join
 
 import auth
 import channels
@@ -32,94 +33,94 @@ def setup_test_interface():
     # Create a channel with the first user.
     channel_id = channels.channels_create(user1['token'], 'channel_1', True)
     
-    return user1['token'], user2['token'], channel_id['channel_id'], user3['token']
+    return user1['token'], user2['u_id'], channel_id['channel_id'], user3['token'], user3['u_id']
 
 # ----- Success Addowner
 def test_addowner_simple(setup_test_interface):
-    user1, user2, channel_id, _ = setup_test_interface
+    tok1, uid2, channel_id, _, _ = setup_test_interface
 
     # Invite the second user to the channel.
-    channel_invite(user1, channel_id, user2)
+    channel_invite(tok1, channel_id, uid2)
 
     # Check that the second user successfully becomes an owner in the channel.
-    assert len(channel_details(user1, channel_id)['owner_members']) == 1
-    assert channel_addowner(user1, channel_id, user2) == {}
-    assert len(channel_details(user1, channel_id)['owner_members']) == 2
+    assert len(channel_details(tok1, channel_id)['owner_members']) == 1
+    assert channel_addowner(tok1, channel_id, uid2) == {}
+    assert len(channel_details(tok1, channel_id)['owner_members']) == 2
 
 def test_flockr_owner(setup_test_interface):
-    user1, user2, channel_id, user3 = setup_test_interface
+    tok1, uid2, channel_1, tok3, uid3 = setup_test_interface
  
-    # Override channel with the second user for testing different admin configurations
-    channel_id = channels.channels_create(user2, 'channel_2', True)
+    # Create new channel with the second user for testing different admin configurations
+    channel_id = channels.channels_create(tok3, 'channel_2', True)
        
     # Invite the second user to the channel.
-    channel_invite(user2, channel_id, user1)
-    channel_invite(user2, channel_id, user3)
+    channel_join(tok1, channel_id)
+    channel_invite(tok1, channel_id, uid2)
 
     # Check that the flockr owner can still make other members owners.
-    assert len(channel_details(user2, channel_id)['owner_members']) == 1
-    assert channel_addowner(user1, channel_id, user3) == {}
-    assert len(channel_details(user2, channel_id)['owner_members']) == 2
+    assert len(channel_details(tok3, channel_id)['owner_members']) == 1
+    assert channel_addowner(tok3, channel_id, uid2) == {}
+    assert len(channel_details(tok1, channel_id)['owner_members']) == 2
 
 # ----- Fail Addowner
 def test_not_member(setup_test_interface):
-    user1, user2, channel_id, _ = setup_test_interface
+    tok1, uid2, channel_id, _, _ = setup_test_interface
 
     # Test that second user is not a member of channel- cannot become owner.
     with pytest.raises(InputError):
-        channel_addowner(user1, channel_id, user2)
+        channel_addowner(tok1, channel_id, uid2)
 
 def test_invalid_token(setup_test_interface):
-    user1, user2, channel_id, _ = setup_test_interface
+    tok1, uid2, channel_id, _, _ = setup_test_interface
     
     # Invite the second user to the channel.
-    channel_invite(user1, channel_id, user2)
+    channel_invite(tok1, channel_id, uid2)
 
     # Check that Access Error is raised when invalid token is used.
     with pytest.raises(AccessError):
-        channel_addowner('fake_token', channel_id, user2)
+        channel_addowner('fake_token', channel_id, uid2)
 
 def test_invalid_user_id(setup_test_interface):
-    user1, user2, channel_id, _ = setup_test_interface
+    tok1, uid2, channel_id, _, _ = setup_test_interface
     
     # Invite the second user to the channel.
-    channel_invite(user1, channel_id, user2)
+    channel_invite(tok1, channel_id, uid2)
 
     # Check that Access Error is raised when invalid token is used.
     with pytest.raises(InputError):
         channel_addowner('fake_token', channel_id, -1)
 
 def test_invalid_channel(setup_test_interface):
-    user1, user2, channel_id, _ = setup_test_interface
+    tok1, uid2, channel_id, tok3, uid3 = setup_test_interface
 
     # Invite the second user to the channel.
-    channel_invite(user1, channel_id, user2)
+    channel_invite(tok1, channel_id, uid2)
 
     # Check that Input Error is raised when invalid channel is used.
     with pytest.raises(InputError):
-        channel_addowner(user1, -1, user2)
+        channel_addowner(tok1, -1, uid2)
 
 def test_not_authorized(setup_test_interface):
-    user1, user2, channel_id, user3 = setup_test_interface
+    tok1, uid2, channel_id, tok3, uid3 = setup_test_interface
 
     # Invite the second & third users to the channel.
-    channel_invite(user1, channel_id, user2)
-    channel_invite(user2, channel_id, user3)
+    channel_invite(tok1, channel_id, uid2)
+    channel_invite(tok1, channel_id, uid3)
 
     # Second member tries to add third member as owner- raise Access Error.
     with pytest.raises(AccessError):
-        channel_addowner(user2, channel_id, user3)
+        channel_addowner(tok3, channel_id, uid2)
 
 def test_already_owner(setup_test_interface):
-    user1, user2, channel_id, user3 = setup_test_interface
+    tok1, uid2, channel_id, tok3, uid3 = setup_test_interface
     
     # Invite the second & third users to the channel.
-    channel_invite(user1, channel_id, user2)
-    channel_invite(user2, channel_id, user3)
+    channel_invite(tok1, channel_id, uid3)
+    channel_invite(tok3, channel_id, uid2)
 
     # Make second user an owner of the channel.
-    channel_addowner(user1, channel_id, user2)
+    channel_addowner(tok1, channel_id, uid2)
 
     # Second user is already an owner- raise Input Error.
     with pytest.raises(InputError):
-        channel_addowner(user1, channel_id, user2)
+        channel_addowner(tok1, channel_id, uid2)
