f6008c9f0ca5ca72114737d46b2d47360a1b5900
Yash <yagasti@gmail.com>
2020-11-05 12:53:12 +1100

    add fixture

---
+++ src/testing_fixtures/channel_test_fixtures.py
@@ -0,0 +1,24 @@
+import pytest
+
+import auth
+import channels
+import other
+
+@pytest.fixture()
+def setup_test_interface():
+    other.clear()
+
+    # Register and login three users.
+    auth.auth_register('validemail1@gmail.com', 'password123', 'fname1', 'lname1')
+    user1 = auth.auth_login('validemail1@gmail.com', 'password123')
+
+    auth.auth_register('validemail2@gmail.com', 'password123', 'fname2', 'lname2')
+    user2 = auth.auth_login('validemail2@gmail.com', 'password123')
+
+    auth.auth_register('validemail3@gmail.com', 'password123', 'fname3', 'lname3')
+    user3 = auth.auth_login('validemail3@gmail.com', 'password123')
+
+    # Create a channel with the first user.
+    channel_id = channels.channels_create(user1['token'], 'channel_1', True)
+
+    yield user1['token'], user2['token'], channel_id['channel_id'], user3['token']
