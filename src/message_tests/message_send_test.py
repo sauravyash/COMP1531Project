e606be01a04e77c45eaffbe4a1c62c1d06c0b0be
Yash <yagasti@gmail.com>
2020-11-06 00:13:52 +1100

    refactored to use fixtures for implementation 2 messages tests

--- src/message_tests/message_send_test.py
+++ src/message_tests/message_send_test.py
@@ -12,84 +12,89 @@ from error import InputError
 from error import AccessError
 
 from message import message_send
+from testing_fixtures.message_test_fixtures import setup_test_interface
 
-def create_test_channel():
-    '''
-    assume message id is an int
-    Create test channel for message send
-    '''
-    other.clear()
-    auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
-    result = auth.auth_login("validemail@gmail.com", "password123")
-
-    channel_id = channels.channels_create(result["token"], "channel_1", True)
-    return (result, channel_id)
-
-
-def test_valid_message_send():
+def test_valid_message_send(setup_test_interface):
     '''
     Sender Token must be valid
     Sender must be a member of the channel
     channel id must be valid
     message letters must be within 1000 characters
     '''
-    result, channel_id = create_test_channel()
-    message_send(result["token"], channel_id["channel_id"], "Funky Monkey")
-    msgs = channel.channel_messages(result["token"], channel_id["channel_id"], 0)
+    user1, user2, user3, channel_dict = setup_test_interface
+
+    tok1 = user1['token']
+    tok2 = user2['token']
+    uid2 = user2['u_id']
+    tok3 = user3['token']
+    channel_id = channel_dict['channel_id']
+
+    message_send(tok1, channel_id, "Funky Monkey")
+    msgs = channel.channel_messages(tok1, channel_id, 0)
 
     assert msgs["messages"][0]['message'] == "Funky Monkey"
 
 
-def test_invalid_message_token():
+def test_invalid_message_token(setup_test_interface):
     '''
     When the sender token is not valid
     '''
-    other.clear
-    auth.auth_register("cool_email@gmail.com", "password123", "fname", "lname")
-    result = auth.auth_login("cool_email@gmail.com", "password123")
+    user1, user2, user3, channel_dict = setup_test_interface
 
-    auth.auth_register("good_email@gmail.com", "password123", "fname1", "lname1")
-    auth.auth_login("good_email@gmail.com", "password123")
-    channel_id = channels.channels_create(result["token"], "channel_1", True)
+    tok1 = user1['token']
+    tok2 = user2['token']
+    uid2 = user2['u_id']
+    tok3 = user3['token']
+    channel_id = channel_dict['channel_id']
 
     with pytest.raises(AccessError):
-        message_send("Dodge", channel_id["channel_id"], "Funky Monkey")
+        message_send("Dodge", channel_id, "Funky Monkey")
 
 
-def test_invalid_message_channel_id():
+def test_invalid_message_channel_id(setup_test_interface):
     '''
     When the message sent to an invalid channel ID
     '''
-    auth.auth_register("coolemail@gmail.com", "password123", "fname", "lname")
-    result = auth.auth_login("coolemail@gmail.com", "password123")
-    channels.channels_create(result["token"], "channel_1", True)
-    with pytest.raises(InputError):
-        message_send(result["token"], 125, "Funky Monkey")
+    user1, user2, user3, channel_dict = setup_test_interface
 
-def test_invalid_message_string_size_1000():
+    tok1 = user1['token']
+    tok2 = user2['token']
+    uid2 = user2['u_id']
+    tok3 = user3['token']
+    channel_id = channel_dict['channel_id']
+
+    with pytest.raises(InputError):
+        message_send(tok1, 125, "Funky Monkey")
+
+def test_invalid_message_string_size_1000(setup_test_interface):
     '''
     When the message exceeds 1000 character limit
     '''
+    user1, user2, user3, channel_dict = setup_test_interface
 
-    result, channel_id = create_test_channel()
+    tok1 = user1['token']
+    tok2 = user2['token']
+    uid2 = user2['u_id']
+    tok3 = user3['token']
+    channel_id = channel_dict['channel_id']
+
     letters = string.ascii_letters
     result_str = ''.join(random.choice(letters) for i in range(1005))
     with pytest.raises(InputError):
-        message_send(result["token"], channel_id["channel_id"], result_str)
+        message_send(tok1, channel_id, result_str)
 
 
-def test_invalid_message_not_in_channel():
+def test_invalid_message_not_in_channel(setup_test_interface):
     '''
     When the sender is not a member of the channel
     '''
-    other.clear()
-    auth.auth_register("validemail@gmail.com", "password123", "fname", "lname")
-    result = auth.auth_login("validemail@gmail.com", "password123")
+    user1, user2, user3, channel_dict = setup_test_interface
 
-    auth.auth_register("good_email@gmail.com", "password123", "fname1", "lname1")
-    result1 = auth.auth_login("good_email@gmail.com", "password123")
-
-    channel_id = channels.channels_create(result["token"], "channel_1", False)
+    tok1 = user1['token']
+    tok2 = user2['token']
+    uid2 = user2['u_id']
+    tok3 = user3['token']
+    channel_id = channels.channels_create(tok1, "channel_1", False)['channel_id']
 
     with pytest.raises(AccessError):
-        message_send(result1["token"], channel_id["channel_id"], "Not in channel")
+        message_send(tok2, channel_id, "Not in channel")
