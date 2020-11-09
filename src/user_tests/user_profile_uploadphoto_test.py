576282f21fcaa161c71b37f9e8b429a0986bfab2
Gabriel Rosendo <g.rosendo@student.unsw.edu.au>
2020-11-09 18:13:19 +1100

    updated user_profile_uploadphoto_test.py function names

--- src/user_tests/user_profile_uploadphoto_test.py
+++ src/user_tests/user_profile_uploadphoto_test.py
@@ -61,21 +61,21 @@ def test_invalid_width_small_end(setup_test_interface):
     with pytest.raises(InputError):
         user_profile_uploadphoto(user["token"], "https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg", 0, 0, -1, 256)
 
-def test_invalid_width_large_start(setup_test_interface):
+def test_invalid_width_large_end(setup_test_interface):
     '''Invalid Dimension'''
     user = setup_test_interface
 
     with pytest.raises(InputError):
         user_profile_uploadphoto(user["token"], "https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg", 0, 0, 10000000, 256)
 
-def test_invalid_height_small_start(setup_test_interface):
+def test_invalid_height_small_end(setup_test_interface):
     '''Invalid Dimension'''
     user = setup_test_interface
 
     with pytest.raises(InputError):
         user_profile_uploadphoto(user["token"], "https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg", 0, 0, 256, -1)
 
-def test_invalid_height_large_start(setup_test_interface):
+def test_invalid_height_large_end(setup_test_interface):
     '''Invalid Dimension'''
     user = setup_test_interface
 
