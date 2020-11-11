e2d6e1eeaec0e9d31c80b3dc4662fde343885b8c
Yash <yagasti@gmail.com>
2020-11-12 01:17:17 +1100

    fixed port to be heroku compatible 3

--- src/server.py
+++ src/server.py
@@ -594,8 +594,8 @@ def svr_clear():
         abort(500)
 
 if __name__ == "__main__":
-    port_num = int(os.getenv('PORT'))
-    port_num = port_num if port_num == None else 0
+    port_num = os.getenv('PORT')
+    port_num = int(port_num) if port_num != None else 0
     print("port: ", port_num)
     APP.run(port=port_num) # Do not edit this port
     #APP.run(port=8080, debug=True) # Debugger
