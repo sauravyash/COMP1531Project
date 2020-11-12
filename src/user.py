998768efbd296a976059ba99eaad45c2412a5090
Gabriel Rosendo <g.rosendo@student.unsw.edu.au>
2020-11-12 14:15:44 +1100

    fixed image naming system

--- src/user.py
+++ src/user.py
@@ -122,7 +122,8 @@ def user_profile_uploadphoto(token, url, x_start, y_start, x_end, y_end):
     if url[-3:] != "jpg":
         raise InputError(description="Image is not jpg")
 
-    img_profile = str(u_id_index + 1) + str(data.generate_img_name(6)) + ".jpg"
+    #img_profile = str(u_id_index + 1) + str(data.generate_img_name(6)) + ".jpg"
+    img_profile = str(data.generate_img_name(6)) + ".jpg"
     filename = "./src/static/profile_images/" + img_profile
 
     try:
